a34eed6f11af2c02eb802b4dcca667e3a1a22c60
NuttyNut2299 <199890687+NuttyNut2299@users.noreply.github.com>
2025-07-16 20:20:37 +0800

    Add hover effect for coloring in js

--- main-js.js
+++ main-js.js
@@ -13,6 +13,9 @@ for (let i = 0; i < 16; i++) {
         const grid = document.createElement("div");
         grid.setAttribute("style", "width: " + defaultGridWidth + "px; height: " + defaultGridHeight + 
             "px; min-width: 0; min-height: 0; flex: 1 1 0; border: solid 1px #e6e6e6; margin: 0; padding: 0;");
+        grid.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "#696969";
+        });
         row.appendChild(grid);
     }
     canvas.appendChild(row);
