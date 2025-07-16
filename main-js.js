29e68c972f6ab9ac2a60374d7f29638fb9281882
NuttyNut2299 <199890687+NuttyNut2299@users.noreply.github.com>
2025-07-16 19:50:41 +0800

    Add default 16x16 flexbox grid to canvas with js

---
+++ main-js.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector("#canvas");
+
+const canvasWidth = canvas.offsetWidth;
+const canvasHeight = canvas.offsetHeight;
+
+const defaultGridWidth = canvasWidth / 16 - 2;
+const defaultGridHeight = canvasHeight / 16 - 2;
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("style", "margin: 0; padding: 0; display: flex; flex: 1 1 0; min-width: 0; min-height: 0");
+    for (let j = 0; j < 16; j++) {
+        const grid = document.createElement("div");
+        grid.setAttribute("style", "width: " + defaultGridWidth + "px; height: " + defaultGridHeight + 
+            "px; min-width: 0; min-height: 0; flex: 1 1 0; border: solid 1px #e6e6e6; margin: 0; padding: 0;");
+        row.appendChild(grid);
+    }
+    canvas.appendChild(row);
+}
