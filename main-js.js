e396dcb4384258cdaffeb85b860e0b8ebad78e38
NuttyNut2299 <199890687+NuttyNut2299@users.noreply.github.com>
2025-07-16 21:08:48 +0800

    Add slider with 1 to 100 range in index.html, Add button click eventlistener funtion to set the grid based on the slider value in js

--- main-js.js
+++ main-js.js
@@ -1,22 +1,46 @@
 const canvas = document.querySelector("#canvas");
 
+// get the total width and height of div canvas
 const canvasWidth = canvas.offsetWidth;
 const canvasHeight = canvas.offsetHeight;
 
-const defaultGridWidth = canvasWidth / 16 - 2;
-const defaultGridHeight = canvasHeight / 16 - 2;
-
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("style", "margin: 0; padding: 0; display: flex; flex: 1 1 0; min-width: 0; min-height: 0");
-    for (let j = 0; j < 16; j++) {
-        const grid = document.createElement("div");
-        grid.setAttribute("style", "width: " + defaultGridWidth + "px; height: " + defaultGridHeight + 
-            "px; min-width: 0; min-height: 0; flex: 1 1 0; border: solid 1px #e6e6e6; margin: 0; padding: 0;");
-        grid.addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor = "#696969";
-        });
-        row.appendChild(grid);
+// set amount of grid based on slider value
+function setGridAmount (sliderValue) {
+    const defaultGridWidth = canvasWidth / sliderValue - 2; // subtract 2 to account for 1px border on each grid
+    const defaultGridHeight = canvasHeight / sliderValue - 2;
+    for (let i = 0; i < sliderValue; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("style", "margin: 0; padding: 0; display: flex; flex: 1 1 0; min-width: 0; min-height: 0");
+        for (let j = 0; j < sliderValue; j++) {
+            const grid = document.createElement("div");
+            grid.setAttribute("style", "width: " + defaultGridWidth + "px; height: " + defaultGridHeight + 
+                "px; min-width: 0; min-height: 0; flex: 1 1 0; border: solid 1px #e6e6e6; margin: 0; padding: 0;");
+            grid.addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor = "#696969";
+            });
+            row.appendChild(grid);
+        }
+        canvas.appendChild(row);
     }
-    canvas.appendChild(row);
 }
+
+var slider = document.querySelector("#grid-range");
+var gridValue = document.querySelector("#grid-value-display");
+gridValue.textContent = "Grid: " + slider.value + " x " + slider.value;
+
+// display current grid slider value dynamically
+slider.oninput = function() {
+    gridValue.textContent = "Grid: " + slider.value + " x " + slider.value;
+}
+
+const btnApply = document.querySelector("#btn-apply");
+
+// set default amount 16 x 16 grid after loading dom
+setGridAmount(slider.value);
+
+btnApply.addEventListener("click", () => {
+    while (canvas.firstChild) {
+        canvas.firstChild.remove(); // remove all grids before resetting the grid
+    }
+    setGridAmount(slider.value);
+});
